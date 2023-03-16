589686cdfaf42d8b655d60a186d54e16b5831edc
bithomie0 <127993563+bithomie0@users.noreply.github.com>
2023-03-16 18:24:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,84 @@
-// ... (previous code) ...
+const canvas = document.getElementById("game");
+const context = canvas.getContext("2d");
 
-// Remove the keyboard event listener
-// document.addEventListener("keydown", (event) => { ...
+const scale = 20;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
 
-// Add touch event listeners for mobile devices
-let touchStartX = 0;
-let touchStartY = 0;
+let snake;
+let fruit;
 
-canvas.addEventListener("touchstart", (event) => {
-    touchStartX = event.touches[0].clientX;
-    touchStartY = event.touches[0].clientY;
+(function setup() {
+  snake = new Snake();
+  fruit = new Fruit();
+
+  fruit.pickLocation();
+
+  window.setInterval(() => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
+    snake.update();
+    snake.draw();
+
+    if (snake.eat(fruit)) {
+      fruit.pickLocation();
+    }
+
+    snake.checkCollision();
+  }, 250);
+})();
+
+document.addEventListener("keydown", (event) => {
+  const direction = event.key.replace("Arrow", "");
+  snake.changeDirection(direction);
 });
 
-canvas.addEventListener("touchmove", (event) => {
-    event.preventDefault();
-    const touchEndX = event.touches[0].clientX;
-    const touchEndY = event.touches[0].clientY;
-    const deltaX = touchEndX - touchStartX;
-    const deltaY = touchEndY - touchStartY;
+canvas.addEventListener("touchstart", (event) => {
+  event.preventDefault();
+  const direction = getSwipeDirection(event.touches[0].clientX, event.touches[0].clientY);
+  if (direction) {
+    snake.changeDirection(direction);
+  }
+});
+
+function getSwipeDirection(startX, startY) {
+  let touchEndX;
+  let touchEndY;
+
+  function handleTouchMove(event) {
+    touchEndX = event.touches[0].clientX;
+    touchEndY = event.touches[0].clientY;
+  }
+
+  function handleTouchEnd() {
+    canvas.removeEventListener("touchmove", handleTouchMove);
+    canvas.removeEventListener("touchend", handleTouchEnd);
+
+    if (!touchEndX || !touchEndY) {
+      return;
+    }
+
+    const deltaX = touchEndX - startX;
+    const deltaY = touchEndY - startY;
     const swipeThreshold = 50;
 
     if (Math.abs(deltaX) > Math.abs(deltaY)) {
-        if (deltaX > swipeThreshold && direction !== "ArrowLeft") {
-            direction = "ArrowRight";
-        } else if (deltaX < -swipeThreshold && direction !== "ArrowRight") {
-            direction = "ArrowLeft";
-        }
+      if (deltaX > swipeThreshold) {
+        return "Right";
+      } else if (deltaX < -swipeThreshold) {
+        return "Left";
+      }
     } else {
-        if (deltaY > swipeThreshold && direction !== "ArrowUp") {
-            direction = "ArrowDown";
-        } else if (deltaY < -swipeThreshold && direction !== "ArrowDown") {
-            direction = "ArrowUp";
-        }
+      if (deltaY > swipeThreshold) {
+        return "Down";
+      } else if (deltaY < -swipeThreshold) {
+        return "Up";
+      }
     }
-});
+  }
 
+  canvas.addEventListener("touchmove", handleTouchMove);
+  canvas.addEventListener("touchend", handleTouchEnd);
+}
+
+// ... (remaining code) ...
