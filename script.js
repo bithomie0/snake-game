a87ab54cf1653eb99cd53bc4d7de6e1efa03f65d
bithomie0 <127993563+bithomie0@users.noreply.github.com>
2023-03-25 17:59:47 +0100

    Update script.js
    Ensuring we have random generation of "Fruit"

--- script.js
+++ script.js
@@ -162,8 +162,10 @@ function Fruit() {
   this.y;
 
   this.pickLocation = function () {
-    this.x = (Math.floor(Math.random() * (columns - 1)) + 1) * scale;
-    this.y = (Math.floor(Math.random() * (rows - 1)) + 1) * scale;
+    this.x = (Math.floor(Math.random() * columns)) * scale;
+    this.y = (Math.floor(Math.random() * rows)) * scale;
+};
+
   };
 
   this.draw = function () {
